cbe51beb9918255d109dc3a9ac7578a656aa1787
Thomas Nilsson <knirch@unixangst.com>
2023-06-08 09:29:48 +0200

    keychron/q8/ansi: realign layers, cosmetic

--- qmk/keyboards/keychron/q8/ansi/keymaps/knirch/keymap.c
+++ qmk/keyboards/keychron/q8/ansi/keymaps/knirch/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q    , KC_W   , KC_E   ,  KC_R  , KC_T   ,               KC_Y   , KC_U   , KC_I   , KC_O   ,    KC_P, KC_LBRC,  KC_RBRC, KC_BSLS,  KC_PGUP,
         KCH_ESC, KC_A    , KC_S   , KC_D   ,  KC_F  , KC_G   ,               KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,  KCH_ENT,           KC_PGDN,
         KC_LSFT, KC_Z    , KC_X   , KC_C   ,  KC_V  , KC_B   ,      KC_DEL , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,             KC_UP,
-        KC_GRV , KC_LALT , KCH_MO1,         KC_LWIN , KCH_MO2,      KC_BSPC,          KC_SPC , KC_RCTL,                             KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_GRV , KC_LALT , KCH_MO1,         KC_LWIN , KCH_MO2,      KC_BSPC, KC_SPC , KC_RCTL,                                      KC_LEFT, KC_DOWN, KC_RGHT),
 
     /* I use the MAC/WIN switch to enable my gaming layer */
     [_BASE_DIP_WIN] = LAYOUT_ansi_69(
@@ -76,21 +76,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q    , KC_W   , KC_E   ,  KC_R  , KC_T   ,               KC_Y   , KC_U   , KC_I   , KC_O   ,    KC_P, KC_LBRC,  KC_RBRC, KC_BSLS,  KC_PGUP,
         KC_LCTL, KC_A    , KC_S   , KC_D   ,  KC_F  , KC_G   ,               KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,  KCH_ENT,           KC_PGDN,
         KC_LSFT, KC_Z    , KC_X   , KC_C   ,  KC_V  , KC_B   ,      KC_DEL , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,             KC_UP,
-        KC_GRV , KC_LALT , KC_LWIN,           KC_SPC, KCH_MO2,      KC_BSPC,          KC_SPC , KC_RCTL,                             KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_GRV , KC_LALT , KC_LWIN,           KC_SPC, KCH_MO2,      KC_BSPC, KC_SPC , KC_RCTL,                                      KC_LEFT, KC_DOWN, KC_RGHT),
 
     [_FN1] = LAYOUT_ansi_69(
-        DB_TOGG, KC_BRID , _______, _______, _______, RGB_VAD, RGB_VAI,               KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,  KC_VOLU,  _______,          QK_BOOT,
-        RGB_TOG, RGB_MOD , RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI,               _______, _______, _______, _______, _______, _______,  _______,  _______,           EE_CLR,
-        _______, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,               KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,  _______,                    DB_TOGG,
+        DB_TOGG, KC_BRID , _______, _______, _______, _______, _______,               KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,  KC_VOLU,  _______,  QK_BOOT,
+        RGB_TOG, RGB_MOD , RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI,               _______, _______, _______, _______, _______, _______,  _______,  _______,   EE_CLR,
+        _______, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,               KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,  _______,            DB_TOGG,
         _______, _______ , _______, _______, _______, _______,      _______, NK_TOGG, _______, _______, _______, _______, _______,            _______,
-        _______, _______ , _______,          _______, _______,      _______,          _______, _______,                              _______, _______,  _______),
+        _______, _______ , _______,          _______, _______,      _______, _______, _______,                                       _______, _______,  _______),
 
     [_FN2] = LAYOUT_ansi_69(
-        DB_TOGG, KC_F1   , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,               KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,   KC_F12,  _______,          QK_BOOT,
-        RGB_TOG, RGB_MOD , RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI,               _______, _______, _______, _______, _______, _______,  _______,  _______,          KC_HOME,
-        KC_CAPS, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,               KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,  _______,          KC_END,
-        _______, _______ , _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______,
-        QK_BOOT, _______ , _______,          _______, _______,      _______,          _______, _______,          _______, _______, _______)
+        DB_TOGG, KC_F1   , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,               KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,   KC_F12,  _______,  QK_BOOT,
+        RGB_TOG, RGB_MOD , RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI,               _______, _______, _______, _______, _______, _______,  _______,  _______,  KC_HOME,
+        KC_CAPS, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,               KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,  _______,             KC_END,
+        _______, _______ , _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______,            _______,
+        QK_BOOT, _______ , _______,          _______, _______,      _______, _______, _______,                                       _______, _______, _______)
 };
 
 
